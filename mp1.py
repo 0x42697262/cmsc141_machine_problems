8f7cdf7b43e77062951ebc9a9a28067a7ddbe0dc
KrulYuno <giogulane.wineffgulane@gmail.com>
2022-03-08 17:58:29 +0900

    test

---
+++ mp1.py
@@ -0,0 +1,33 @@
+class Set_mp:
+    def __init__(self):
+        pass
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def subset(self):
+        pass
+
+    def union(self):
+        pass
+
+    def intersection(self):
+        pass
+
+    def difference(self):
+        pass
+
+    def power_set(self):
+        pass
+
+
+def main():
+    print({"hello", "there", "human"})
+    pass
+
+
+if __name__ == '__main__':
+    main()
